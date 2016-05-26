6d500ced0154d89ea6c684f88b19b1cae5930b14
Connor Sheehan <sheehacg@mcmaster.ca>
2016-05-26 10:40:46 -0400

    Add requirements.txt, make setup.py read it for consistency

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup
 
+with open('requirements.txt', 'r') as reqs_txt:
+    reqs = reqs_txt.readlines()
+
 setup(
     name='pulse-notify',
     version='0.2',
     packages=['pulsenotify'],
-    url='',
+    url='http://github.com/cgsheeh/pulse-notify',
     license='',
     author='Connor Sheehan',
     author_email='csheehan@mozilla.com',
     description='',
-    install_requires=[
-        'aioamqp',
-        'aiohttp',
-        'blessings',
-        'boto3',
-        'uvloop',
-        'jinja2',
-    ],
+    install_requires=reqs,
     entry_points="""
         [console_scripts]
         pulse-notify = pulsenotify.main:cli

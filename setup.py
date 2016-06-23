6d486e094724aaa7ada97a8ad01a004abf9bbc07
Connor Sheehan <sheehacg@mcmaster.ca>
2016-06-23 15:06:32 -0400

    Add readme to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 with open('requirements.txt', 'r') as reqs_txt:
     reqs = reqs_txt.readlines()
 
+with open('README.md', 'r') as readme_txt:
+    readme = reqs_txt.read()
+
 setup(
     name='pulse-notify',
     version='0.2',
@@ -12,6 +15,7 @@ setup(
     author='Connor Sheehan',
     author_email='csheehan@mozilla.com',
     description='Sends notifications based on taskcluster task statuses.',
+    long_description=readme,
     install_requires=reqs,
     entry_points="""
         [console_scripts]

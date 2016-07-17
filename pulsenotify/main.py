c5da2a84f74f6d17416db87786777826f99f95d9
Connor Sheehan <sheehacg@mcmaster.ca>
2016-07-17 00:10:23 -0400

    Remove coloured log formatting. Set id_config load option to command line.

--- pulsenotify/main.py
+++ pulsenotify/main.py
@@ -8,51 +8,24 @@ from pulsenotify.worker import worker
 
 
 log = logging.getLogger(__name__)
-
-
-class ColoredFormatter(logging.Formatter):
-    """
-    Credits: https://gist.github.com/exhuma/8147910
-    """
-
-    def __init__(self, terminal, *args, **kwargs):
-        super(ColoredFormatter, self).__init__(*args, **kwargs)
-        self._terminal = terminal
-
-    def format(self, record):
-        output = super(ColoredFormatter, self).format(record)
-        if record.levelno >= logging.CRITICAL:
-            line_color = self._terminal.bold_yellow_on_red
-        elif record.levelno >= logging.ERROR:
-            line_color = self._terminal.red
-        elif record.levelno >= logging.WARNING:
-            line_color = self._terminal.yellow
-        elif record.levelno >= logging.INFO:
-            line_color = self._terminal.green
-        else:
-            line_color = self._terminal.white
-        return line_color(output)
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s;\t%(levelname)s;\t%(message)s')
 
 
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", dest="loglevel",
-                        action="store_const", const=logging.DEBUG,
-                        default=logging.INFO)
+    parser.add_argument('-env', '--environment', default='dev', nargs='?',
+                        help='Specifies development or production environment',
+                        dest='env', type=str)
+
     args = parser.parse_args()
 
-    terminal = Terminal()
-    clifmt = ColoredFormatter(
-        terminal,
-        '%(asctime)s [%(threadName)s] %(levelname)-10s %(message)s')
     root_logger = logging.getLogger()
     clihandler = logging.StreamHandler(sys.stdout)
-    clihandler.setFormatter(clifmt)
     root_logger.setLevel(logging.NOTSET)
     root_logger.addHandler(clihandler)
 
     try:
-        event_loop.run_until_complete(worker(NotifyConsumer()))
+        event_loop.run_until_complete(worker(NotifyConsumer(args.env)))
         event_loop.run_forever()
     except KeyboardInterrupt as ke:
         # TODO: make better shutdown

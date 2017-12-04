787384babcdbf8268c126115d1cf33f9d45057bd
Rok Garbas <rok@garbas.si>
2017-12-04 13:33:30 +0100

    flake8 gardening

--- pulsenotify/consumer.py
+++ pulsenotify/consumer.py
@@ -241,7 +241,7 @@ class TaskData(object):
                     's3_key': self.make_s3_key(run_number, build_properties),
                     'destination_url': self.S3_DESTINATION.format(bucket=os.environ['S3_BUCKET'],
                                                                   log_key=self.make_s3_key(run_number)),
-            }
+                }
 
     def make_s3_key(self, run_id, build_properties):
         task_metadata = self.definition['metadata']
